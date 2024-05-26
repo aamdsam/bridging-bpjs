e75f7ec8dd28389c22ed66126e4a55cc05ac7176
AamDsam <muh.muammar@gmail.com>
2024-05-26 16:07:23 +0800

    tambah Alergi dan Prognosa

---
+++ src/PCare/Alergi.php
@@ -0,0 +1,11 @@
+<?php namespace AamDsam\Bpjs\PCare;
+
+use AamDsam\Bpjs\PCare\PcareService;
+
+class Alergi extends PcareService
+{
+    /**
+     * @var string
+     */
+    protected $feature = 'alergi';
+}
---
+++ src/PCare/Prognosa.php
@@ -0,0 +1,11 @@
+<?php namespace AamDsam\Bpjs\PCare;
+
+use AamDsam\Bpjs\PCare\PcareService;
+
+class Prognosa extends PcareService
+{
+    /**
+     * @var string
+     */
+    protected $feature = 'prognosa';
+}
